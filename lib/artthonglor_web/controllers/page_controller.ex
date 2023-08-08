3dbfba5bc13e2be889ae1e5d5d55ed23fda7cfff
Suracheth Chawla <me@suracheth.com>
2023-08-08 23:29:58 +0700

    Add meta descriptions

--- lib/artthonglor_web/controllers/page_controller.ex
+++ lib/artthonglor_web/controllers/page_controller.ex
@@ -2,22 +2,25 @@ defmodule ArtthonglorWeb.PageController do
   use ArtthonglorWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home, page_title: "ราคาดีที่สุด...ในทองหล่อ")
+    render(conn, :home,
+      page_title: "Best Price in Thonglor",
+      description: "Condominium Registered and Ready To Move In. Best Price in Thonglor"
+    )
   end
 
   def room_plan(conn, _params) do
-    render(conn, :room_plan, page_title: "Room Plan")
+    render(conn, :room_plan, page_title: "Room Plan", description: "Room Plan")
   end
 
   def floor_plan(conn, _params) do
-    render(conn, :floor_plan, page_title: "Floor Plan")
+    render(conn, :floor_plan, page_title: "Floor Plan", description: "Floor Plan")
   end
 
   def contact(conn, _params) do
-    render(conn, :contact, page_title: "Contact")
+    render(conn, :contact, page_title: "Contact", description: "Contact")
   end
 
   def gallery(conn, _params) do
-    render(conn, :gallery, page_title: "Gallery")
+    render(conn, :gallery, page_title: "Gallery", description: "Gallery")
   end
 end
